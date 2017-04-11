f6d8602ffa29b8fe2643664c79428f7bfe6a0319
dylanberry <dylan@dylanberry.com>
2017-04-11 14:05:27 -0400

    Update jQuery-FixAnchorLinks.js

--- javascript/jQuery-FixAnchorLinks.js
+++ javascript/jQuery-FixAnchorLinks.js
@@ -1,12 +1,25 @@
-// jQuery passed in as $ to an Immediately Invoked Function Expression (IIFE)
-(function($){
-   $(document).ready(function() {
-      // overwrite anchor links
-      $("a").each(function() {
-        var a = $(this);
-        if (a.href) a.href = a.href.replace("#/", "#");
+<!-- BEGIN Prevent iMIS/AngularJS Location Provider from breaking the anchor links -->
+<!-- Place this script near the end of the body -->
+<script type="text/javascript">
+   // iMIS 20.2.49 fix for AngularJS $location adding a forward slash in anchor links
+   // ie.:	before:	https://girlguides.ca/WEB/GGC/Join_Us/Provincial_Contacts/GGC/Join_Us/Provincial_Contacts.aspx?hkey=efa89cae-7c45-41c1-b8d7-2452537f5c9b#SK
+   //  	after:	https://girlguides.ca/WEB/GGC/Join_Us/Provincial_Contacts/GGC/Join_Us/Provincial_Contacts.aspx?hkey=efa89cae-7c45-41c1-b8d7-2452537f5c9b#/SK
+   // jQuery passed in as $ to an Immediately Invoked Function Expression (IIFE)
+   (function($){
+      $(document).ready(function() {
+        angular.module('app')
+            .config(function ($locationProvider) {
+              $locationProvider.html5Mode({
+                  enabled: true,
+                  rewriteLinks: false
+               });
+         });
+
+        $("a").each(function() {
+         var a = $(this);
+         if (a.href) a.href = a.href.replace("#/", "#");
+        });
       });
-      // overwrite the current url's anchor
-      window.location.hash = window.location.hash.replace("#/", "#");
-   });
-})(jQuery)
+   })(jQuery)
+ </script>
+<!-- END -->
