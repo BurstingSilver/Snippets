b0a7bafd1577e549743f4bee1bafdc6966bbf02d
dylanberry <dylan@dylanberry.com>
2017-04-12 08:14:33 -0400

    Update jQuery-FixAnchorLinks.js
    Updated url examples to be more generic

--- javascript/jQuery-FixAnchorLinks.js
+++ javascript/jQuery-FixAnchorLinks.js
@@ -2,8 +2,8 @@
 <!-- Place this script near the end of the body in the master page -->
 <script type="text/javascript">
    // iMIS 20.2.49 fix for AngularJS $location adding a forward slash in anchor links
-   // ie.:	before:	https://girlguides.ca/WEB/GGC/Join_Us/Provincial_Contacts/GGC/Join_Us/Provincial_Contacts.aspx?hkey=efa89cae-7c45-41c1-b8d7-2452537f5c9b#SK
-   //  	after:	https://girlguides.ca/WEB/GGC/Join_Us/Provincial_Contacts/GGC/Join_Us/Provincial_Contacts.aspx?hkey=efa89cae-7c45-41c1-b8d7-2452537f5c9b#/SK
+   // ie.:	before:	https://site.com/WEB/Regions.aspx#SK
+   //  	after:	https://site.com/WEB/Regions.aspx#/SK
    // jQuery passed in as $ to an Immediately Invoked Function Expression (IIFE)
    (function($){
       $(document).ready(function() {
