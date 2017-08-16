bc606fe24d1407657f6eeb0c9521186b30a700a3
dylanberry <dylan@dylanberry.com>
2017-08-16 11:34:09 -0400

    Create jQuery-SchedulerRest.js

---
+++ javascript/jQuery-SchedulerRest.js
@@ -0,0 +1,25 @@
+// jQuery passed in as $ to an Immediately Invoked Function Expression (IIFE)
+(function($){
+    var schedulerPath = "Asi.Scheduler_Web;
+    var schedulerUrl = "https://" + window.location.hostname + "/" + schedulerPath;
+    var username = "MANAGER";
+    var password = "password";
+    $.ajax({
+        method: "POST",
+        url: schedulerUrl + "/token",
+        data: "grant_type=password&username=" + username + "&password=" + password
+    })
+    .done(function(data) {
+        $.ajax({
+            method: "GET",
+            url: schedulerUrl + "/api/Party",
+            headers:
+            {
+                "authorization": "Bearer " + data.access_token
+            },
+        })
+        .done(function(data) {console.log(data);})
+        .fail(function(data) {console.log(data);});
+     })
+    .fail(function(data) {console.log(data);});
+})(jQuery)
