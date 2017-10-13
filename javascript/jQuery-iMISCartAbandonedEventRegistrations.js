f2dd939f9b99428dbb7a5866d0654830baf55ebb
James <jschwartz@burstingsilver.com>
2017-10-13 16:30:40 -0400

    Update jQuery-iMISCartAbandonedEventRegistrations.js

--- javascript/jQuery-iMISCartAbandonedEventRegistrations.js
+++ javascript/jQuery-iMISCartAbandonedEventRegistrations.js
@@ -7,61 +7,63 @@ var registrantIds = "";
 (function($){
 	console.log("Starting Processing");
 	ProcessData(500, 0, $);
-})(jQuery)
- 
-function ProcessData(limit, offset, $){
-	$.ajax({
-    	"method": "get",
-    	"url": gWebRoot + "/api/Cart?Limit=" + limit + "&offset=" + offset,
-    	"headers": { "RequestVerificationToken" : $("#__RequestVerificationToken").val()}
-	})
-	.done(function(data) {
-    	var cartItems = data.Items.$values;
-    	var cartCount = Object.keys(data.Items.$values).length;
-    	if (cartCount > 0)
-    	{
-        	ProcessData(limit, offset + limit, $);
-    	} else {
-        	console.log("Finished Processing");
-        	console.log("Found " + eventRecordCount + " records that registered for event " + eventId);
-        	console.log("Registrant Ids list: " + registrantIds);
-    	}
- 
-    	ProcessResults(data);
-	})
-	.fail(function(data) {console.log(data);})
-}
- 
-function ProcessResults(data){
-	//var userID = 241658;
-	var cartItems = data.Items.$values;
-	var cartCount = Object.keys(data.Items.$values).length;
-//    	console.log("Returned " + cartCount + " records from Cart");
- 
-	for (j=0;j<=cartCount;j++){
-    	if (data.Items.$values[j] != null){
-        	var orderLines = data.Items.$values[j].ComboOrder.Order.Lines;
-        	var imisId = data.Items.$values[j].UserId;
-        	var lineCount = Object.keys(orderLines.$values).length;
-	//        console.log("line count: " + lineCount + " imisid: " + imisId);
- 
-        	for (i=0;i<=lineCount;i++)
-        	{
-            	var orderLine = orderLines.$values[i];
-            	if (orderLine != null){
-//                    	console.log("Event id: " + orderLine.Event.EventId);
-                	if (eventId == orderLine.Event.EventId){
-                    	//console.log("ID: " + imisId);
-                    	if (registrantIds == "")
-                        	registrantIds = '"' + imisId + '"';
-                    	else
-                        	registrantIds += ',"' + imisId + '"';
-                        	
-                    	eventRecordCount++;
-                    	break;
-                	}
-            	}
-        	}
-    	}
+	
+
+	function ProcessData(limit, offset, $){
+		$.ajax({
+		"method": "get",
+		"url": gWebRoot + "/api/Cart?Limit=" + limit + "&offset=" + offset,
+		"headers": { "RequestVerificationToken" : $("#__RequestVerificationToken").val()}
+		})
+		.done(function(data) {
+		var cartItems = data.Items.$values;
+		var cartCount = Object.keys(data.Items.$values).length;
+		if (cartCount > 0)
+		{
+			ProcessData(limit, offset + limit, $);
+		} else {
+			console.log("Finished Processing");
+			console.log("Found " + eventRecordCount + " records that registered for event " + eventId);
+			console.log("Registrant Ids list: " + registrantIds);
+		}
+
+		ProcessResults(data);
+		})
+		.fail(function(data) {console.log(data);})
 	}
-}
+
+	function ProcessResults(data){
+		//var userID = 241658;
+		var cartItems = data.Items.$values;
+		var cartCount = Object.keys(data.Items.$values).length;
+	//    	console.log("Returned " + cartCount + " records from Cart");
+
+		for (j=0;j<=cartCount;j++){
+		if (data.Items.$values[j] != null){
+			var orderLines = data.Items.$values[j].ComboOrder.Order.Lines;
+			var imisId = data.Items.$values[j].UserId;
+			var lineCount = Object.keys(orderLines.$values).length;
+		//        console.log("line count: " + lineCount + " imisid: " + imisId);
+
+			for (i=0;i<=lineCount;i++)
+			{
+			var orderLine = orderLines.$values[i];
+			if (orderLine != null){
+	//                    	console.log("Event id: " + orderLine.Event.EventId);
+				if (eventId == orderLine.Event.EventId){
+				//console.log("ID: " + imisId);
+				if (registrantIds == "")
+					registrantIds = '"' + imisId + '"';
+				else
+					registrantIds += ',"' + imisId + '"';
+
+				eventRecordCount++;
+				break;
+				}
+			}
+			}
+		}
+		}
+	}
+})(jQuery)
+
