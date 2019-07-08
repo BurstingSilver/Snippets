f97fd8d4cc9e8ae29bbb1aeb361e7f52b8889e7f
davey11 <dtrieu@burstingsilver.com>
2019-07-08 08:40:51 -0600

    Update Windows-iMIS-Build.ps1

--- PowerShell/Windows-iMIS-Build.ps1
+++ PowerShell/Windows-iMIS-Build.ps1
@@ -60,15 +60,6 @@ function Disable-IEESC
 <#*****************************************************************************************
 Main Program Start
 *****************************************************************************************#>
-function Disable-IEESC
-{
-    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
-    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-    Stop-Process -Name Explorer
-    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
-}
 # windows explorer settings show file extensions
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
 
