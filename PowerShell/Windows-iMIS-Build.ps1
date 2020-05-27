e6df89db0cdeadce9696877d8bea0d24878ce204
davey11 <dtrieu@burstingsilver.com>
2020-05-27 09:50:19 -0600

    Update Windows-iMIS-Build.ps1

--- PowerShell/Windows-iMIS-Build.ps1
+++ PowerShell/Windows-iMIS-Build.ps1
@@ -73,7 +73,7 @@ if ($Reinstall -ne "True")
 }
 
 #Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Set Chocolatey Safe Switch
 chocolatey feature enable -n allowGlobalConfirmation
