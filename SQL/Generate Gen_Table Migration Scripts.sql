020c9cee4f4320a4d6bac37c7b4bd1ef52aa39ed
Justin Trout <jtrout@burstingsilver.com>
2021-11-02 11:22:36 -0600

    Update Generate Gen_Table Migration Scripts.sql
    Fixed accidental overwrite with shortcut script

--- SQL/Generate Gen_Table Migration Scripts.sql
+++ SQL/Generate Gen_Table Migration Scripts.sql
@@ -1,33 +1,15 @@
-/* 
-    USAGE: 
-    Set the @websiteRiSEName to the name of the RiSE site to pull all shortcuts for that site.
-    If you're unsure of the site name, check the Perspective table.
-    You can also set @websiteRiSEName to null to pull shortcuts for "All Sites"
-*/
-
-DECLARE @websiteRiSEName nvarchar(100) = 'Staff';
+DECLARE @TableName varchar(60) = 'TABLE_NAME';
 
 SELECT 
-	'IF NOT EXISTS (SELECT 1 FROM URLMapping WHERE URLMappingKey = ''' + CAST(URLMapping.URLMappingKey AS nvarchar(38)) + ''')' + CHAR(13) + CHAR(10) + 
-	'    INSERT INTO URLMapping(URLMappingKey,DirectoryName,URL,WebsiteDocumentVersionKey,TargetDocumentVersionKey,URLMappingDesc,URLParameters,URLMappingTypeCode) VALUES (' + 
-	'''' + CAST(URLMapping.URLMappingKey AS nvarchar(38)) + ''',' + 
-	'''' + URLMapping.DirectoryName + ''',' + 
-	'''' + URLMapping.[URL] + ''',' + 
-	IIF(URLMapping.WebsiteDocumentVersionKey IS NULL,'NULL','''' + CAST(URLMapping.WebsiteDocumentVersionKey AS nvarchar(38)) + '''') + ',' + 
-	IIF(URLMapping.TargetDocumentVersionKey IS NULL,'NULL','''' + CAST(URLMapping.TargetDocumentVersionKey AS nvarchar(38)) + '''') + ',' + 
-	IIF(URLMapping.URLMappingDesc IS NULL,'NULL','''' + URLMapping.URLMappingDesc + '''') + ',' + 
-	IIF(URLMapping.URLParameters IS NULL,'NULL','''' + URLMapping.URLParameters + '''') + ',' + 
-	+ CAST(URLMapping.URLMappingTypeCode AS nvarchar(2)) + ');' 
+	'IF NOT EXISTS (SELECT 1 FROM Gen_Tables WHERE Gen_Tables.TABLE_NAME = ''' + Gen_Tables.TABLE_NAME + ''' AND Gen_Tables.CODE = ''' + Gen_Tables.CODE + ''')' + CHAR(13) + CHAR(10) + 
+	'    INSERT INTO Gen_Tables (TABLE_NAME, CODE, SUBSTITUTE, UPPER_CODE, [DESCRIPTION], NCODE) VALUES (' + 
+	'''' + TABLE_NAME + ''', ' + 
+	'''' + REPLACE(CODE,'''','''''') + ''', ' + 
+	'''' + REPLACE(SUBSTITUTE,'''','''''') + ''', ' + 
+	'''' + REPLACE(UPPER_CODE,'''','''''') + ''', ' + 
+	'''' + REPLACE([DESCRIPTION],'''','''''') + ''', ' + 
+	'''' + NCODE + ''');' 
 FROM 
-	URLMapping 
+	Gen_Tables 
 WHERE 
-	(@websiteRiSEName IS NULL AND 
-	URLMapping.WebsiteDocumentVersionKey IS NULL) OR  
-	EXISTS (
-		SELECT 
-			1 
-		FROM 
-			Perspective 
-		WHERE 
-			Perspective.WebsiteKey = URLMapping.WebsiteDocumentVersionKey AND 
-			Perspective.PerspectiveName = @websiteRiSEName);
+	TABLE_NAME = @TableName;
