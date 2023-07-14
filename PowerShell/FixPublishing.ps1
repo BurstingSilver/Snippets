67dc65b9df8c4f529b9172c9a5e61a6615614f28
Jamie Steele <steele.jamie1991@gmail.com>
2023-07-14 12:26:36 -0400

    Update FixPublishing.ps1

--- PowerShell/FixPublishing.ps1
+++ PowerShell/FixPublishing.ps1
@@ -6,11 +6,19 @@ param (
     [Parameter()]
     [string]$Username,
     [Parameter()]
-    [string]$Password,
+    [pscredential]$Password,
     [Parameter()]
     [string]$AsiSchedulerAppPool,
     [Parameter()]
-    [string]$InstancePath
+    [string]$InstancePath,
+    [switch]
+    $SkipClearLucene,
+    [switch]
+    $ClearTaskQueues,
+    [switch]
+    $SkipRecreatePublishing,
+    [switch]
+    $SkipStartScheduler
 )
 
 function Stop-AppPool ($webAppPoolName,[int]$secs) {
@@ -34,40 +42,66 @@ function Stop-AppPool ($webAppPoolName,[int]$secs) {
     return $retvalue
 }
 
-# Stop AsiScheduler app pool
-if((Get-WebAppPoolState -Name $AsiSchedulerAppPool).Value -ne "Stopped"){
-    Stop-AppPool $AsiSchedulerAppPool 120
+function Stop-Scheduler ($AsiSchedulerAppPool) {
+    if((Get-WebAppPoolState -Name $AsiSchedulerAppPool).Value -ne "Stopped"){
+        Stop-AppPool $AsiSchedulerAppPool 120
+    }
 }
+
+function Start-Scheduler ($AsiSchedulerAppPool) {
+    if((Get-WebAppPoolState -Name $AsiSchedulerAppPool).Value -ne "Started"){
+        Write-Output ("Starting Application Pool: {0}" -f $AsiSchedulerAppPool)
+        Start-WebAppPool -Name $AsiSchedulerAppPool
+    }
+}
+
+function Clear-Lucene ($InstancePath) {
+    Write-Output ("Clearing content of lucene folder")
+    $LucenePath = Join-Path -path $InstancePath -childpath "indexServiceProtected\Search\Lucene"
+    Get-ChildItem -Path $LucenePath -Include *.* -File -Recurse | ForEach-Object { $_.Delete()}
+}
+
+function Clear-Task-Queues ($ServerInstance, $Database, $Username, [PSCredential] $Password) {
+    Write-Output ("Clearing task queues")
+    Invoke-Sqlcmd -Query "DELETE FROM TaskQueuePublishDetail" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
+    Invoke-Sqlcmd -Query "DELETE FROM TaskQueue WHERE TaskQueueTypeId = 1 and TaskQueueId NOT IN (SELECT TaskQueueId FROM TaskQueuePublishDetail)" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
+    Invoke-Sqlcmd -Query "DELETE FROM TaskQueueTriggerDetail" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password   
+    Invoke-Sqlcmd -Query "DELETE FROM PublishMessageLog" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
+    Invoke-Sqlcmd -Query "DELETE FROM PublishRequestDetail" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
+}
+
+function Reset-Publishing-Service ($ServerInstance, $Database, $Username, [PSCredential] $Password) {
+    Write-Output ("Recreating publishing queue and service broker")
+    Invoke-Sqlcmd -Query "DECLARE @name VARCHAR(MAX) DECLARE db_cursor CURSOR FOR SELECT name FROM sys.service_queues WHERE is_ms_shipped != 1 OPEN db_cursor FETCH NEXT FROM db_cursor INTO @name WHILE @@FETCH_STATUS = 0 BEGIN PRINT CONCAT('Dropping Service Broker Service & Queue: ', @name) DECLARE @cmd VARCHAR(1000) = CONCAT('DROP SERVICE [', IIF(@name = 'iMISPublishQueue', 'iMISPublishService', @name), ']', ' DROP QUEUE [', @name, ']') EXEC (@cmd) FETCH NEXT FROM db_cursor INTO @name END CLOSE db_cursor DEALLOCATE db_cursor" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
+    Invoke-Sqlcmd -Query "EXEC asi_EnsurePublishQueueAndServiceBroker" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
+    Invoke-Sqlcmd -Query "IF NOT EXISTS (SELECT * FROM sys.services WHERE name = N'iMISPublishService') BEGIN CREATE SERVICE iMISPublishService ON QUEUE iMISPublishQueue ([http://schemas.microsoft.com/SQL/Notifications/PostQueryNotification]); END" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
+}
+
+# Stop AsiScheduler app pool
+Stop-Scheduler $AsiSchedulerAppPool
 
 # Clear lucene index files
-Write-Output ("Clearing content of lucene folder")
-$LucenePath = Join-Path -path $InstancePath -childpath "indexServiceProtected\Search\Lucene"
-Get-ChildItem -Path $LucenePath -Include *.* -File -Recurse | foreach { $_.Delete()}
-
-# Clear Task Queues etc..
-Invoke-Sqlcmd -Query "DELETE FROM TaskQueuePublishDetail WHERE ItemAction != 2" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
-
-Invoke-Sqlcmd -Query "DELETE FROM TaskQueue WHERE TaskQueueTypeId = 1 and TaskQueueId NOT IN (SELECT TaskQueueId FROM TaskQueuePublishDetail)" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
-
-Invoke-Sqlcmd -Query "DELETE FROM PublishMessageLog WHERE PublishRequestDetailKey NOT IN (SELECT PublishRequestDetailKey FROM PublishRequestDetail WHERE PublishedDateTime IS NOT NULL)" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
-Invoke-Sqlcmd -Query "DELETE FROM PublishRequestDetail WHERE PublishedDateTime IS NOT NULL" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
-Invoke-Sqlcmd -Query "DELETE FROM PublishRequest WHERE PublishRequestKey NOT IN (SELECT PublishRequestKey FROM PublishRequestDetail)" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
-
-Invoke-Sqlcmd -Query "UPDATE TaskQueueTriggerDetail SET TaskQueueStatusId = 1 WHERE TaskQueueStatusId = 2 AND StatusUpdatedOn < DATEADD(hour, -12, dbo.asi_GetAppDatetime())" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
-
-# Recreate publishing queue and service broker
-Write-Output ("Recreating publishing queue and service broker")
-Invoke-Sqlcmd -Query "IF EXISTS(SELECT * FROM sys.services WHERE name = N'iMISPublishService') DROP SERVICE iMISPublishService; IF EXISTS (SELECT * FROM sys.service_queues WHERE name = N'iMISPublishQueue') DROP QUEUE iMISPublishQueue;" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
-Invoke-Sqlcmd -Query "CREATE QUEUE iMISPublishQueue; CREATE SERVICE iMISPublishService ON QUEUE iMISPublishQueue([http://schemas.microsoft.com/SQL/Notifications/PostQueryNotification]);" -ServerInstance $ServerInstance -Database $Database -Username $Username -Password $Password
-
-# Start AsiScheduler app pool
-if((Get-WebAppPoolState -Name $AsiSchedulerAppPool).Value -ne "Started"){
-    Write-Output ("Starting Application Pool: {0}" -f $AsiSchedulerAppPool)
-    Start-WebAppPool -Name $AsiSchedulerAppPool
+if ($PSBoundParameters.ContainsKey("SkipClearLucene")) {
+    Write-Output ("Skiping Clearing Lucene Folder")
+} else {
+    Clear-Lucene $InstancePath
 }
 
+# Clear Task Queues etc..
+if($PSBoundParameters.ContainsKey("ClearTaskQueues")) {
+    Clear-Task-Queues $ServerInstance $Database $Username $Password
+}
 
+# Recreate publishing queue and service broker
+if($PSBoundParameters.ContainsKey("SkipRecreatePublishing")) {
+    Write-Output ("Skiping Recreate publishing queue and service broker")
+} else {
+    Reset-Publishing-Service $ServerInstance $Database $Username $Password
+}
 
-
-
-
+# Start AsiScheduler app pool
+if($PSBoundParameters.ContainsKey("SkipStartScheduler")) {
+    Write-Output ("Skiping StartScheduler")
+} else {
+    Start-Scheduler $AsiSchedulerAppPool
+}
